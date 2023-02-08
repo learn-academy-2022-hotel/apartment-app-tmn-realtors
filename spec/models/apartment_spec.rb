27f463e8371669a083f19d0f921353e1bf897703
TorinJacobs <potours@yahoo.com>
2023-02-08 12:04:34 -0800

    updated specs

--- spec/models/apartment_spec.rb
+++ spec/models/apartment_spec.rb
@@ -2,47 +2,158 @@ require "rails_helper"
 
 RSpec.describe Apartment, type: :model do
   it "should validate street" do
-    apartment = Apartment.create
+    apartment = Apartment.create( 
+      city: "Bikini Bottom",
+      state: "Pacific Ocean",
+      manager: "Mustachio Jones",
+      email: "mjones@example.com",
+      price: "1000 sand dollars",
+      bedrooms: 2,
+      bathrooms: 2,
+      pets: "yes",
+      image: "https://images.thedailystar.net/sites/default/files/styles/very_big_201/public/feature/images/who_lives_in_a_pineapple_under_the_sea.jpg?itok=iYr37hhG"
+    )
     expect(apartment.errors[:street]).to_not be_empty
   end
   it "should validate city" do
-    apartment = Apartment.create
+    apartment = Apartment.create( 
+      street: "Coral Lane",
+      state: "Pacific Ocean",
+      manager: "Mustachio Jones",
+      email: "mjones@example.com",
+      price: "1000 sand dollars",
+      bedrooms: 2,
+      bathrooms: 2,
+      pets: "yes",
+      image: "https://images.thedailystar.net/sites/default/files/styles/very_big_201/public/feature/images/who_lives_in_a_pineapple_under_the_sea.jpg?itok=iYr37hhG"
+    )    
     expect(apartment.errors[:city]).to_not be_empty
   end
   it "should validate state" do
-    apartment = Apartment.create
+    apartment = Apartment.create( 
+      street: "Coral Lane",
+      city: "Bikini Bottom",
+      manager: "Mustachio Jones",
+      email: "mjones@example.com",
+      price: "1000 sand dollars",
+      bedrooms: 2,
+      bathrooms: 2,
+      pets: "yes",
+      image: "https://images.thedailystar.net/sites/default/files/styles/very_big_201/public/feature/images/who_lives_in_a_pineapple_under_the_sea.jpg?itok=iYr37hhG"
+    )    
     expect(apartment.errors[:state]).to_not be_empty
   end
   it "should validate manager" do
-    apartment = Apartment.create
+    apartment = Apartment.create( 
+      street: "Coral Lane",
+      city: "Bikini Bottom",
+      state: "Pacific Ocean",
+      email: "mjones@example.com",
+      price: "1000 sand dollars",
+      bedrooms: 2,
+      bathrooms: 2,
+      pets: "yes",
+      image: "https://images.thedailystar.net/sites/default/files/styles/very_big_201/public/feature/images/who_lives_in_a_pineapple_under_the_sea.jpg?itok=iYr37hhG"
+    )     
     expect(apartment.errors[:manager]).to_not be_empty
   end
   it "should validate email" do
-    apartment = Apartment.create
+    apartment = Apartment.create( 
+      street: "Coral Lane",
+      city: "Bikini Bottom",
+      state: "Pacific Ocean",
+      manager: "Mustachio Jones",
+      price: "1000 sand dollars",
+      bedrooms: 2,
+      bathrooms: 2,
+      pets: "yes",
+      image: "https://images.thedailystar.net/sites/default/files/styles/very_big_201/public/feature/images/who_lives_in_a_pineapple_under_the_sea.jpg?itok=iYr37hhG"
+    )  
     expect(apartment.errors[:email]).to_not be_empty
   end
   it "should validate price" do
-    apartment = Apartment.create
+    apartment = Apartment.create( 
+      street: "Coral Lane",
+      city: "Bikini Bottom",
+      state: "Pacific Ocean",
+      manager: "Mustachio Jones",
+      email: "mjones@example.com",
+      bedrooms: 2,
+      bathrooms: 2,
+      pets: "yes",
+      image: "https://images.thedailystar.net/sites/default/files/styles/very_big_201/public/feature/images/who_lives_in_a_pineapple_under_the_sea.jpg?itok=iYr37hhG"
+    ) 
     expect(apartment.errors[:price]).to_not be_empty
   end
   it "should validate bedrooms" do
-    apartment = Apartment.create
+    apartment = Apartment.create( 
+      street: "Coral Lane",
+      city: "Bikini Bottom",
+      state: "Pacific Ocean",
+      manager: "Mustachio Jones",
+      email: "mjones@example.com",
+      price: "1000 sand dollars",
+      bathrooms: 2,
+      pets: "yes",
+      image: "https://images.thedailystar.net/sites/default/files/styles/very_big_201/public/feature/images/who_lives_in_a_pineapple_under_the_sea.jpg?itok=iYr37hhG"
+    )     
     expect(apartment.errors[:bedrooms]).to_not be_empty
   end
   it "should validate bathrooms" do
-    apartment = Apartment.create
+    apartment = Apartment.create( 
+      street: "Coral Lane",
+      city: "Bikini Bottom",
+      state: "Pacific Ocean",
+      manager: "Mustachio Jones",
+      email: "mjones@example.com",
+      price: "1000 sand dollars",
+      bedrooms: 2,
+      pets: "yes",
+      image: "https://images.thedailystar.net/sites/default/files/styles/very_big_201/public/feature/images/who_lives_in_a_pineapple_under_the_sea.jpg?itok=iYr37hhG"
+    )    
     expect(apartment.errors[:bathrooms]).to_not be_empty
   end
   it "should validate pets" do
-    apartment = Apartment.create
+    apartment = Apartment.create( 
+      street: "Coral Lane",
+      city: "Bikini Bottom",
+      state: "Pacific Ocean",
+      manager: "Mustachio Jones",
+      email: "mjones@example.com",
+      price: "1000 sand dollars",
+      bedrooms: 2,
+      bathrooms: 2,
+      image: "https://images.thedailystar.net/sites/default/files/styles/very_big_201/public/feature/images/who_lives_in_a_pineapple_under_the_sea.jpg?itok=iYr37hhG"
+    )    
     expect(apartment.errors[:pets]).to_not be_empty
   end
   it "should validate image" do
-    apartment = Apartment.create
+    apartment = Apartment.create( 
+      street: "Coral Lane",
+      city: "Bikini Bottom",
+      state: "Pacific Ocean",
+      manager: "Mustachio Jones",
+      email: "mjones@example.com",
+      price: "1000 sand dollars",
+      bedrooms: 2,
+      bathrooms: 2,
+      pets: "yes",    
+    )    
     expect(apartment.errors[:image]).to_not be_empty
   end
   it "should validate user_id" do
-    apartment = Apartment.create
+    apartment = Apartment.create( 
+      street: "Coral Lane",
+      city: "Bikini Bottom",
+      state: "Pacific Ocean",
+      manager: "Mustachio Jones",
+      email: "mjones@example.com",
+      price: "1000 sand dollars",
+      bedrooms: 2,
+      bathrooms: 2,
+      pets: "yes",  
+      image: "https://images.thedailystar.net/sites/default/files/styles/very_big_201/public/feature/images/who_lives_in_a_pineapple_under_the_sea.jpg?itok=iYr37hhG"
+    )    
     expect(apartment.errors[:user_id]).to_not be_empty
   end
 end
