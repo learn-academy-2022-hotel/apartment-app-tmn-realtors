60bf645d8b20ed60fe992637e899a00ce228a99f
Nata19754 <chavesnatally0419@gmail.com>
2023-02-10 08:47:15 -0800

    fixed indentation

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,27 +1,34 @@
 class ApartmentsController < ApplicationController
-    def index
-        apartments = Apartment.all
-        render json: apartments
+  def index
+    apartments = Apartment.all
+    render json: apartments
+  end
+
+  def create
+    apartment = Apartment.create(apartment_params)
+    if apartment.valid?
+      render json: apartment
+    else
+      render json: apartment.errors, status: 422
     end
-    def create
-        apartment = Apartment.create(apartment_params)
-            if apartment.valid?
-                render json: apartment
-            else
-                render json: apartment.errors, status: 422
-            end      
+  end
+
+  def update
+    apartment = Apartment.find(params[:id])
+    apartment.update(apartment_params)
+    if apartment.valid?
+      render json: apartment
+    else
+      render json: apartment.errors, status: 422
     end
-    def update
-        apartment = Apartment.find(params[:id])
-        apartment.update(apartment_params)
-        render json: apartment
-    end
-    def destroy
-    
-    end
-    
-    private
-    def apartment_params
-        params.require(:apartment).permit(:street, :city, :state, :manager, :email, :price, :bedrooms, :bathrooms, :pets, :image, :user_id)
-    end    
+  end
+
+  def destroy
+  end
+
+  private
+
+  def apartment_params
+    params.require(:apartment).permit(:street, :city, :state, :manager, :email, :price, :bedrooms, :bathrooms, :pets, :image, :user_id)
+  end
 end
--- spec/models/apartment_spec.rb
+++ spec/models/apartment_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe Apartment, type: :model do
   it "should validate street" do
-    apartment = Apartment.create( 
+    apartment = Apartment.create(
       city: "Bikini Bottom",
       state: "Pacific Ocean",
       manager: "Mustachio Jones",
@@ -16,7 +16,7 @@ RSpec.describe Apartment, type: :model do
     expect(apartment.errors[:street]).to_not be_empty
   end
   it "should validate city" do
-    apartment = Apartment.create( 
+    apartment = Apartment.create(
       street: "Coral Lane",
       state: "Pacific Ocean",
       manager: "Mustachio Jones",
@@ -26,11 +26,11 @@ RSpec.describe Apartment, type: :model do
       bathrooms: 2,
       pets: "yes",
       image: "https://images.thedailystar.net/sites/default/files/styles/very_big_201/public/feature/images/who_lives_in_a_pineapple_under_the_sea.jpg?itok=iYr37hhG"
-    )    
+    )
     expect(apartment.errors[:city]).to_not be_empty
   end
   it "should validate state" do
-    apartment = Apartment.create( 
+    apartment = Apartment.create(
       street: "Coral Lane",
       city: "Bikini Bottom",
       manager: "Mustachio Jones",
@@ -40,11 +40,11 @@ RSpec.describe Apartment, type: :model do
       bathrooms: 2,
       pets: "yes",
       image: "https://images.thedailystar.net/sites/default/files/styles/very_big_201/public/feature/images/who_lives_in_a_pineapple_under_the_sea.jpg?itok=iYr37hhG"
-    )    
+    )
     expect(apartment.errors[:state]).to_not be_empty
   end
   it "should validate manager" do
-    apartment = Apartment.create( 
+    apartment = Apartment.create(
       street: "Coral Lane",
       city: "Bikini Bottom",
       state: "Pacific Ocean",
@@ -54,11 +54,11 @@ RSpec.describe Apartment, type: :model do
       bathrooms: 2,
       pets: "yes",
       image: "https://images.thedailystar.net/sites/default/files/styles/very_big_201/public/feature/images/who_lives_in_a_pineapple_under_the_sea.jpg?itok=iYr37hhG"
-    )     
+    )
     expect(apartment.errors[:manager]).to_not be_empty
   end
   it "should validate email" do
-    apartment = Apartment.create( 
+    apartment = Apartment.create(
       street: "Coral Lane",
       city: "Bikini Bottom",
       state: "Pacific Ocean",
@@ -68,11 +68,11 @@ RSpec.describe Apartment, type: :model do
       bathrooms: 2,
       pets: "yes",
       image: "https://images.thedailystar.net/sites/default/files/styles/very_big_201/public/feature/images/who_lives_in_a_pineapple_under_the_sea.jpg?itok=iYr37hhG"
-    )  
+    )
     expect(apartment.errors[:email]).to_not be_empty
   end
   it "should validate price" do
-    apartment = Apartment.create( 
+    apartment = Apartment.create(
       street: "Coral Lane",
       city: "Bikini Bottom",
       state: "Pacific Ocean",
@@ -82,11 +82,11 @@ RSpec.describe Apartment, type: :model do
       bathrooms: 2,
       pets: "yes",
       image: "https://images.thedailystar.net/sites/default/files/styles/very_big_201/public/feature/images/who_lives_in_a_pineapple_under_the_sea.jpg?itok=iYr37hhG"
-    ) 
+    )
     expect(apartment.errors[:price]).to_not be_empty
   end
   it "should validate bedrooms" do
-    apartment = Apartment.create( 
+    apartment = Apartment.create(
       street: "Coral Lane",
       city: "Bikini Bottom",
       state: "Pacific Ocean",
@@ -96,11 +96,11 @@ RSpec.describe Apartment, type: :model do
       bathrooms: 2,
       pets: "yes",
       image: "https://images.thedailystar.net/sites/default/files/styles/very_big_201/public/feature/images/who_lives_in_a_pineapple_under_the_sea.jpg?itok=iYr37hhG"
-    )     
+    )
     expect(apartment.errors[:bedrooms]).to_not be_empty
   end
   it "should validate bathrooms" do
-    apartment = Apartment.create( 
+    apartment = Apartment.create(
       street: "Coral Lane",
       city: "Bikini Bottom",
       state: "Pacific Ocean",
@@ -110,11 +110,11 @@ RSpec.describe Apartment, type: :model do
       bedrooms: 2,
       pets: "yes",
       image: "https://images.thedailystar.net/sites/default/files/styles/very_big_201/public/feature/images/who_lives_in_a_pineapple_under_the_sea.jpg?itok=iYr37hhG"
-    )    
+    )
     expect(apartment.errors[:bathrooms]).to_not be_empty
   end
   it "should validate pets" do
-    apartment = Apartment.create( 
+    apartment = Apartment.create(
       street: "Coral Lane",
       city: "Bikini Bottom",
       state: "Pacific Ocean",
@@ -124,11 +124,11 @@ RSpec.describe Apartment, type: :model do
       bedrooms: 2,
       bathrooms: 2,
       image: "https://images.thedailystar.net/sites/default/files/styles/very_big_201/public/feature/images/who_lives_in_a_pineapple_under_the_sea.jpg?itok=iYr37hhG"
-    )    
+    )
     expect(apartment.errors[:pets]).to_not be_empty
   end
   it "should validate image" do
-    apartment = Apartment.create( 
+    apartment = Apartment.create(
       street: "Coral Lane",
       city: "Bikini Bottom",
       state: "Pacific Ocean",
@@ -137,12 +137,12 @@ RSpec.describe Apartment, type: :model do
       price: "1000 sand dollars",
       bedrooms: 2,
       bathrooms: 2,
-      pets: "yes",    
-    )    
+      pets: "yes"
+    )
     expect(apartment.errors[:image]).to_not be_empty
   end
   it "should validate user_id" do
-    apartment = Apartment.create( 
+    apartment = Apartment.create(
       street: "Coral Lane",
       city: "Bikini Bottom",
       state: "Pacific Ocean",
@@ -151,9 +151,9 @@ RSpec.describe Apartment, type: :model do
       price: "1000 sand dollars",
       bedrooms: 2,
       bathrooms: 2,
-      pets: "yes",  
+      pets: "yes",
       image: "https://images.thedailystar.net/sites/default/files/styles/very_big_201/public/feature/images/who_lives_in_a_pineapple_under_the_sea.jpg?itok=iYr37hhG"
-    )    
+    )
     expect(apartment.errors[:user_id]).to_not be_empty
   end
 end
